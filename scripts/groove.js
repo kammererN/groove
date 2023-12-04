77a104d46ae850286ee3c3a125f987310aaf298b
antonio1121 <90068472+antonio1121@users.noreply.github.com>
2023-12-03 19:49:28 -0500

    prototype

--- scripts/groove.js
+++ scripts/groove.js
@@ -1,2 +1,12 @@
-// hello
-const hi = 23 ;
+import * as Tone from 'tone'
+
+const playButton = document.getElementById("play-button");
+const sound = new Tone.Synth().toDestination();
+
+
+playButton.addEventListener("click", () => {
+    if(Tone.context.state != "running") {
+        Tone.start();
+    }
+    sound.triggerAttackRelease("E2", "4n");
+})
