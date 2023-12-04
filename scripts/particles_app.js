4105eaa81f566c12b80f3df1ad7fb842ec5c678e
Nick Kammerer <56482517+nxrada@users.noreply.github.com>
2023-12-04 17:22:32 -0500

    Added background animation.

---
+++ scripts/particles_app.js
@@ -0,0 +1,4 @@
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+particlesJS.load('particle-canvas', 'config/particles.json', function() {
+  console.log('callback - particles_app.js config loaded');
+});
